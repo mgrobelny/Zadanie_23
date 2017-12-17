ac70c058ade5206dfb3b9ae6f210e9297dda42c0
mgrobelny <michalmaciejg@gmail.com>
2017-12-17 18:58:08 +0100

    init

---
+++ server/controllers/kanban.controller.js
@@ -0,0 +1,10 @@
+import Kanban from '../models/kanban';
+
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+};
